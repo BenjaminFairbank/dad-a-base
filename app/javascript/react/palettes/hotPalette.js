62bbd10c307916f8a90de0e38f483a7d823fbdbe
BenjaminFairbank <BenFairbank26@gmail.com>
2020-11-17 13:45:20 +0800

    Theme trimmed & palettes, styles => separate files

---
+++ app/javascript/react/palettes/hotPalette.js
@@ -0,0 +1,20 @@
+const hotPalette = {
+  type: 'dark',
+  primary: {
+    main: '#314455',
+  },
+  secondary: {
+    main: '#644E5B',
+  },
+  tertiary: {
+    main: '#9E5A63'
+  },
+  quaternary: {
+    main: '#C96567'
+  },
+  quinary: {
+    main: '#97AABD'
+  },
+}
+
+export default hotPalette
